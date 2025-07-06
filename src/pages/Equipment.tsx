f168c60fd6d31e0e19182bab43cb1ab8fa2c6df9
Greg-goprod <contact@gc-entertainment.ch>
2025-07-06 12:34:34 +0200

    Fix Card component import

--- src/pages/Equipment.tsx
+++ src/pages/Equipment.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useAppContext } from '../contexts/AppContext';
-import { Card } from '../components/common/Card';
+import Card from '../components/common/Card';
 import Button from '../components/common/Button';
 import Badge from '../components/common/Badge';
 import { Modal } from '../components/common/Modal';

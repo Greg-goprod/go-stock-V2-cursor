7139af681cc1270f7eb4e392feda17b05bcd3191
Greg-goprod <contact@gc-entertainment.ch>
2025-07-06 12:51:01 +0200

    Fix ExcelImport import error

--- src/pages/Equipment.tsx
+++ src/pages/Equipment.tsx
@@ -14,7 +14,7 @@ import CheckoutModal from '../components/checkout/CheckoutModal';
 import { ReturnModal } from '../components/checkout/ReturnModal';
 import { FilterPanel } from '../components/common/FilterPanel';
 import ConfirmModal from '../components/common/ConfirmModal';
-import { ExcelImport } from '../components/import/ExcelImport';
+import ExcelImport from '../components/import/ExcelImport';
 import { Plus, QrCode, Wrench, History, LogOut, LogIn, Edit, Trash2, Download, Upload, Search, Filter } from 'lucide-react';
 import { Equipment as EquipmentType, EquipmentInstance } from '../types';
 import { useStatusColors } from '../hooks/useStatusColors';
